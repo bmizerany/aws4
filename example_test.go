910b3553921f39886610b7abbe7b02f5a6c6401c
Blake Mizerany <blake.mizerany@gmail.com>
2013-05-23 15:33:52 -0700

    introducting Transport

--- example_test.go
+++ example_test.go
@@ -11,20 +11,20 @@ import (
 	"strings"
 )
 
+var keys = &aws4.Keys{
+	AccessKey: os.Getenv("AWS_ACCESS_KEY"),
+	SecretKey: os.Getenv("AWS_SECRET_KEY"),
+}
+
+func init() {
+	http.DefaultTransport.(*http.Transport).RegisterProtocol("aws4", &aws4.Transport{Keys: keys})
+}
+
 func Example_jSONBody() {
 	data := strings.NewReader("{}")
-	r, _ := http.NewRequest("POST", "https://dynamodb.us-east-1.amazonaws.com/", data)
+	r, _ := http.NewRequest("POST", "aws4://dynamodb.us-east-1.amazonaws.com/", data)
 	r.Header.Set("Content-Type", "application/x-amz-json-1.0")
 	r.Header.Set("X-Amz-Target", "DynamoDB_20111205.ListTables")
-
-	tk := &aws4.Keys{
-		AccessKey: os.Getenv("AWS_ACCESS_KEY"),
-		SecretKey: os.Getenv("AWS_SECRET_KEY"),
-	}
-
-	if err := aws4.Sign(tk, r); err != nil {
-		log.Fatal(err)
-	}
 
 	resp, err := http.DefaultClient.Do(r)
 	if err != nil {
@@ -37,24 +37,12 @@ func Example_jSONBody() {
 }
 
 func Example_formEncodedBody() {
-	r, _ := http.NewRequest("POST", "https://autoscaling.us-east-1.amazonaws.com/", nil)
-	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
 	v := make(url.Values)
 	v.Set("Action", "DescribeAutoScalingGroups")
 
-	r.Body = ioutil.NopCloser(strings.NewReader(v.Encode()))
-
-	tk := &aws4.Keys{
-		AccessKey: os.Getenv("AWS_ACCESS_KEY"),
-		SecretKey: os.Getenv("AWS_SECRET_KEY"),
-	}
-
-	if err := aws4.Sign(tk, r); err != nil {
-		log.Fatal(err)
-	}
-
-	resp, err := http.DefaultClient.Do(r)
+	u := "aws4://autoscaling.us-east-1.amazonaws.com/"
+	body := ioutil.NopCloser(strings.NewReader(v.Encode()))
+	resp, err := http.Post(u, "application/x-www-form-urlencoded", body)
 	if err != nil {
 		log.Fatal(err)
 	}
