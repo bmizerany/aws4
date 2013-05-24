c5750918cfbbe58a546b032e9a4c4506815a8c7d
Blake Mizerany <blake.mizerany@gmail.com>
2013-05-23 17:02:15 -0700

    introduce Client

--- example_test.go
+++ example_test.go
@@ -3,22 +3,11 @@ package aws4_test
 import (
 	"fmt"
 	"github.com/bmizerany/aws4"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 	"strings"
 )
-
-func init() {
-	log.SetFlags(log.Lshortfile)
-}
-
-var keys = &aws4.Keys{
-	AccessKey: os.Getenv("AWS_ACCESS_KEY"),
-	SecretKey: os.Getenv("AWS_SECRET_KEY"),
-}
 
 func Example_jSONBody() {
 	data := strings.NewReader("{}")
@@ -26,7 +15,7 @@ func Example_jSONBody() {
 	r.Header.Set("Content-Type", "application/x-amz-json-1.0")
 	r.Header.Set("X-Amz-Target", "DynamoDB_20111205.ListTables")
 
-	resp, err := aws4.Do(r, keys)
+	resp, err := aws4.DefaultClient.Do(r)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,9 +29,7 @@ func Example_formEncodedBody() {
 	v := make(url.Values)
 	v.Set("Action", "DescribeAutoScalingGroups")
 
-	url := "https://autoscaling.us-east-1.amazonaws.com/"
-	body := ioutil.NopCloser(strings.NewReader(v.Encode()))
-	resp, err := aws4.Post(url, "application/x-www-form-urlencoded", body, keys)
+	resp, err := aws4.PostForm("https://autoscaling.us-east-1.amazonaws.com/", v)
 	if err != nil {
 		log.Fatal(err)
 	}
