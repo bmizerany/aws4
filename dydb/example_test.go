419f5ab0cc1f4bf13fba534bcb4cdaee1b95e3aa
Blake Mizerany <blake.mizerany@gmail.com>
2013-05-26 18:32:08 -0700

    update example

--- dydb/example_test.go
+++ dydb/example_test.go
@@ -13,12 +13,47 @@ func init() {
 func Example_listTables() {
 	db := new(dydb.DB)
 
+	type AttributeDefinition struct {
+		AttributeName string
+		AttributeType string
+	}
+
+	type KeySchema struct {
+		AttributeName string
+		KeyType       string
+	}
+
+	type CreateTable struct {
+		TableName             string
+		AttributeDefinitions  []AttributeDefinition
+		KeySchema             []KeySchema
+		ProvisionedThroughput struct {
+			ReadCapacityUnits  int
+			WriteCapacityUnits int
+		}
+	}
+
+	posts := new(CreateTable)
+	posts.TableName = "Posts"
+	posts.AttributeDefinitions = []AttributeDefinition{{"Slug", "S"}}
+	posts.KeySchema = []KeySchema{{"Slug", "HASH"}}
+	posts.ProvisionedThroughput.ReadCapacityUnits = 4
+	posts.ProvisionedThroughput.WriteCapacityUnits = 4
+
+	if err := db.Exec("CreateTable", posts); err != nil {
+		if e, ok := err.(*dydb.ResponseError); ok {
+			if e.TypeName() != "ResourceInUseException" {
+				log.Fatal(err)
+			}
+		}
+	}
+
 	var resp struct{ TableNames []string }
-	if err := db.Do("ListTables", nil).Decode(&resp); err != nil {
+	if err := db.Query("ListTables", nil).Decode(&resp); err != nil {
 		log.Fatal(err)
 	}
 
 	// Output:
-	// ["Users", "Posts"]
+	// ["Posts"]
 	fmt.Printf("%q", resp.TableNames)
 }
